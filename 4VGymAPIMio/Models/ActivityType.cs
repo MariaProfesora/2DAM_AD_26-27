733387e9b4e6104d8801266719722bba0f2af869
MariaProfesora <maria_martin@cuatrovientos.es>
2026-09-09 14:08:26 +0200

    ErrorResponse y Comentarios

---
+++ 4VGymAPIMio/Models/ActivityType.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _4VGymAPI.Models
+{
+    public class ActivityType
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("number-monitors")]
+        public int NumberMonitors { get; set; }
+
+    }
+}
---
+++ 4VGymAPIMio/Models/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _4VGymAPI.Models
+{
+
+    /// <summary>
+    /// Representa la estructura de error según la especificación OpenAPI de 4VGym.
+    /// </summary>
+    public class ErrorResponse
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
